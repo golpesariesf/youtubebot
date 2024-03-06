8fd9e7fb15a9ac365b8d77bf5116f13ea4a0e893
Your Name <golpesariesf@gmail.com>
2024-03-06 16:47:33 +0300

    add all

--- main.py
+++ main.py
@@ -89,5 +89,6 @@ def webhook():
     bot.set_webhook(url=APP_URL)
     return '!', 200
 
-if __name__ == '__main__' or __name__ == 'telegram_bot':
-    generate_and_print_uuid()
+if __name__ == '__main__':
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8090)))
+
