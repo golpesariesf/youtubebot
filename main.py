97f29ced78589fdf79b1461a026ec9fc23cfeaca
Your Name <golpesariesf@gmail.com>
2024-03-09 02:15:52 +0300

    host

--- main.py
+++ main.py
@@ -137,7 +137,5 @@ def verify_ipn_signature(data):
 
 
 if __name__ == "__main__":
-
- server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
     bot.polling()
-    s
