28fdbedd1f49cf35f58b28271af7e00e1c02bcbd
Your Name <golpesariesf@gmail.com>
2024-03-09 10:54:57 +0300

    dsd

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import logging
 import requests
 import uuid
 from coinpayments import CoinPaymentsAPI
-
+import os
 
 
 # Setup logging
@@ -132,4 +132,5 @@ def verify_ipn_signature(data):
 
 
 if __name__ == "__main__":
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
     bot.polling()
