963806a254aa1407f41dda1a2d201c5e4612f0ab
Your Name <golpesariesf@gmail.com>
2024-03-10 01:52:28 +0300

    code

--- main.py
+++ main.py
@@ -1,29 +1,60 @@
-﻿from coinpayments import CoinPaymentsAPI
-import uuid
-import telebot
+﻿import requests
+import hashlib
+import json
 from flask import Flask, request
 import os
-import requests
-import json
-import hmac
-import hashlib
 import logging
-from telebot import types
+from telebot import types, TeleBot
 
-
-# Setup logging ggggg
 logging.basicConfig(filename='bot_log.txt', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 TOKEN = '7095077129:AAE-rDWtk6q7S8ZgkxmcfLtnJdMtAYJutq4'
 APP_URL = f'https://youtubenew-c7c31f2cda46.herokuapp.com/{TOKEN}'
 
-bot = telebot.TeleBot(TOKEN)
+bot = TeleBot(TOKEN)
 server = Flask(__name__)
 COINPAYMENTS_PUBLIC_KEY = '616e319dad674f8906f129a735d299d6665388a0fe3f4e075ffc3e2b9c3ce8f3'
 COINPAYMENTS_PRIVATE_KEY = 'D544Edec2fa5725C5913C5806665393ec58769563f5C7477DfBb8A8C4302867b'
 IPN_SECRET = '1122334455667788aA@'
 
-api = CoinPaymentsAPI(public_key=COINPAYMENTS_PUBLIC_KEY, private_key=COINPAYMENTS_PRIVATE_KEY)
+class CoinPaymentsAPI:
+    def __init__(self, public_key, private_key):
+        self.public_key = public_key
+        self.private_key = private_key
+
+    def check_payment_status(self, txn_id):
+        response = self.api_call('get_tx_info', {'txid': txn_id})
+
+        if 'status' not in response:
+            return 'Invalid response from CoinPayments API.'
+
+        if response['status'] == 100:
+            return 'Payment successful!'
+        elif response['status'] < 0:
+            return 'Payment failed.'
+        else:
+            return 'Payment is still processing.'
+
+    def api_call(self, cmd, req={}):
+        if not self.public_key or not self.private_key:
+            return {'error': 'You have not set your public and private keys!'}
+
+        req['version'] = 1
+        req['cmd'] = cmd
+        req['key'] = self.public_key
+        req['format'] = 'json'
+
+        post_data = '&'.join([f'{key}={req[key]}' for key in req])
+        hmac_value = hashlib.sha512(post_data.encode('utf-8'), self.private_key.encode('utf-8')).hexdigest()
+        req['HMAC'] = hmac_value
+
+        response = requests.post('https://www.coinpayments.net/api.php', data=req)
+
+        try:
+            data = response.json()
+            return data
+        except json.JSONDecodeError:
+            return {'error': 'Unable to parse JSON result.'}
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -36,10 +67,9 @@ def start(message):
 
     bot.reply_to(message, "To make a payment, click on the button below:", reply_markup=markup)
 
-
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo(message):
-    txn_id = message.text.strip()  # Use request.text instead of message.text
+    txn_id = message.text.strip()
     user_id = message.from_user.id
 
     if not txn_id or len(txn_id) != 32:
@@ -47,53 +77,10 @@ def echo(message):
         return
 
     # Check payment status
-    result = check_payment_status(txn_id)
+    result = api.check_payment_status(txn_id)
 
     # Notify the user based on the result
     bot.send_message(user_id, result)
 
-def check_payment_status(txn_id):
-    response = api.get_tx_info(txn_id)
-    
-    if response['status'] == 100:
-        return 'Payment successful!'
-    elif response['status'] < 0:
-        return 'Payment failed.'
-    else:
-        return 'Payment is still processing.'
-
-@server.route('/' + TOKEN, methods=['POST'])
-def get_message():
-    json_string = request.get_data().decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return '!', 200
-
-@server.route('/')
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url=APP_URL)
-    return '!', 200
-
-@server.route('/ipn', methods=['POST'])
-def handle_ipn():
-    ipn = request.get_json()
-    if not ipn:
-        return 'Invalid IPN', 400
-
-    if not api.verify_ipn(ipn, IPN_SECRET):
-        return 'Invalid IPN', 400
-
-    # در اینجا شما می‌توانید وضعیت پرداخت را بررسی کنید و به کاربر اطلاع دهید
-    # برای مثال، شما می‌توانید از api.get_tx_info استفاده کنید تا اطلاعات تراکنش را بدست آورید
-    tx_info = api.get_tx_info(ipn['txn_id'])
-    if tx_info['status'] == 100:
-        print('پرداخت کامل شده است')
-    elif tx_info['status'] < 0:
-        print('پرداخت ناموفق بود')
-    else:
-        print('پرداخت در حال پردازش است')
-
-    return 'OK'
 if __name__ == '__main__':
     server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
