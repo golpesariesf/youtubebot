eb4222b6520e4445af9b92053de7990b16984907
Your Name <golpesariesf@gmail.com>
2024-03-10 20:20:23 +0300

    dddd

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-﻿import uuid
+﻿from coinpayments import CoinPaymentsAPI
+import uuid
 import telebot
 from flask import Flask, request
 import os
+import requests
+import json
+import hmac
+import hashlib
+import logging
+from telebot import types
 
-TOKEN = '7137673728:AAE85wL1RBYskkrlCZaIzhEbgKmiEBiefDI'
+
+TOKEN = '7095077129:AAE-rDWtk6q7S8ZgkxmcfLtnJdMtAYJutq4'
 APP_URL = f'https://youtubenew-c7c31f2cda46.herokuapp.com/{TOKEN}'
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
@@ -17,8 +25,14 @@ def generate_and_print_uuid():
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
-    generate_and_print_uuid()
+
+    # Create an inline keyboard with a link button
+    markup = types.InlineKeyboardMarkup()
+    link_button = types.InlineKeyboardButton("Click here to pay", url=f'https://www.coinpayments.net/index.php?cmd=_pay&reset=1&merchant=c80ec2928c4b6836e6ada19db1c229ec&item_name=iphone15&currency=USD&amountf=25.00000000&quantity=1&allow_quantity=0&want_shipping=0&allow_extra=1&')
+    markup.add(link_button)
+
+    bot.reply_to(message, "To make a payment, click on the button below:", reply_markup=markup)
+
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo(message):
